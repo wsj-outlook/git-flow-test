75556c2347b2f7a62f149039f1938ec7e08ecfdd
weishunjie-github <157680333@qq.com>
2023-02-20 14:20:12 +0800

    branch1-gitrebase

---
+++ gitrebase.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: weishunjie-github 157680333@qq.com
+ * @Date: 2023-02-20 14:11:14
+ * @LastEditors: weishunjie-github 157680333@qq.com
+ * @LastEditTime: 2023-02-20 14:19:43
+ * @FilePath: \git-flow-test\gitrebase.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 从main创建一个分支branch1  并添加  gitreabse.js 文件 三次提交
+// 用另一个账号也从main拉一个branch2 并添加 gitreabse2.js 文件  三次提交
+
+let obj = {
+    name:'branch1-gitrebase'
+}
