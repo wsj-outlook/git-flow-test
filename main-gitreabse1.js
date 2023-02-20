1100631ef2d3e37a44242524d355226055f7d585
weishunjie-github <157680333@qq.com>
2023-02-20 15:42:27 +0800

    main-gitreabse1

---
+++ main-gitreabse1.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: weishunjie-github 157680333@qq.com
+ * @Date: 2023-02-20 15:02:36
+ * @LastEditors: weishunjie-github 157680333@qq.com
+ * @LastEditTime: 2023-02-20 15:41:58
+ * @FilePath: \git-flow-test\main-gitreabse.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+//新建两个账号的分支后，并且提交三个commit 并且提交推送各自的分支。
+
+//此处为了main 提交三个文件。并且push 种类文件命名都是随意的。
+
+let obj = {
+    name:'main-gitreabse1',
+    fli:['main.js','main.css','main.html']
+
+}
+
